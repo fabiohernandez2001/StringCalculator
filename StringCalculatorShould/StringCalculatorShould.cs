4fc81314e1dce635d8072eea2cc40c39cc80e04e
fabiohernandez2001 <fabiohmaru@gmai.com>
2023-10-11 16:12:34 +0100

    Problemas con la compilación de los test, step 2 completado

--- StringCalculatorShould/StringCalculatorShould.cs
+++ StringCalculatorShould/StringCalculatorShould.cs
@@ -5,6 +5,7 @@ namespace StringCalculatorTest
     {
         private StringCalculator.stringCalculator calculator =
         new StringCalculator.stringCalculator();
+
         [TestMethod]
         public void when_adding_should_return_sum()
         {
@@ -18,6 +19,13 @@ namespace StringCalculatorTest
             string empty = "";
             int result = calculator.add(empty);
             Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void when_more_than_three_numbers_should_return_result()
+        {
+            string many = "1,2,3,4,5,6";
+            int result = calculator.add(many);
+            Assert.AreEqual(21, result);
         }
     }
 }
